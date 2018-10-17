d320e513b39b28b2550054b184f95e848d4c7bdd
skerschb <sue.kerschbaumer@10gen.com>
2018-10-17 18:20:49 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,162 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import LockIcon from '@material-ui/icons/LockOutlined';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Stitch, RemoteMongoClient, AnonymousCredential } from "mongodb-stitch-browser-sdk";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const classes = theme => ({
+  layout: {
+    display: 'block', // Fix IE11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+  },
+  avatar: {
+    margin: theme.spacing.unit,
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE11 issue.
+    marginTop: theme.spacing.unit,
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3,
+  },
+});
+
+
+class Demo extends React.Component {
+
+  constructor(props) {
+    console.log("call constructor");
+    super(props);
+    this.state = {email: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+     this.setState({email: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log("handle submit");
+    this.setState({validate: true});
+  }
+
+  setupStitch() {
+    const appName = 'ref_data-bnbxq';
+    const credential = new GoogleRedirectCredential("https://skerschb.github.io/testauth/")
+    Stitch.defaultAppClient.auth.loginWithRedirect(credential);
+  }
+
+  componentDidMount() {
+    console.log("component Did mount running");
+    this.setupStitch();
+    this.stitchClient.callFunction('fetchDocuments', ['ref_data/atlas_api', {path: { $regex: /^\/api\/atlas/}}]).then((response) => {
+      this.setState({
+        documents: response,
+        currentDetails: [],
+      })
+    });
+  }
+
+
+  componentDidUpdate() {
+    console.log("Parameters component updated");
+  }
+
+  set(value) {
+    this.setState({"hello":"duh"});
+    console.log(this.state);
+  }
+
+  //this.propTypes = {
+  //  classes: PropTypes.object.isRequired,
+  //};
+
+
+  render() {
+
+  return (
+    <React.Fragment>
+      <div className="login">
+      <div className="login-container">
+      <CssBaseline />
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form}>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="email">Email Address</InputLabel>
+              <Input id="email" onChange={this.handleChange} value={this.state.email} name="email" autoComplete="email" autoFocus />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input
+                name="password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </FormControl>
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              component="button"
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign in
+            </Button>
+          </form>
+        </Paper>
+      </main>
+      </div>
+      </div>
+    </React.Fragment>
+  )
+}
+}
+
+// ========================================
+
+  ReactDOM.render(
+  <Demo />,
+  document.getElementById('root')
+);
