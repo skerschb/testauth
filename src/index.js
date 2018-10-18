a9be6b9b2444968473af659c45382ec74856b429
skerschb <sue.kerschbaumer@10gen.com>
2018-10-18 11:35:55 -0400

    test

--- src/index.js
+++ src/index.js
@@ -1,52 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import LockIcon from '@material-ui/icons/LockOutlined';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import withStyles from '@material-ui/core/styles/withStyles';
 import './index.css';
 
-import { Stitch, GoogleRedirectCredential, RemoteMongoClient, AnonymousCredential } from "mongodb-stitch-browser-sdk";
-
-const classes = theme => ({
-  layout: {
-    display: 'block', // Fix IE11 issue.
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-      width: 400,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-  },
-  paper: {
-    marginTop: theme.spacing.unit * 8,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
-  },
-  avatar: {
-    margin: theme.spacing.unit,
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE11 issue.
-    marginTop: theme.spacing.unit,
-  },
-  submit: {
-    marginTop: theme.spacing.unit * 3,
-  },
-});
+import { Stitch, GoogleRedirectCredential, RemoteMongoClient } from "mongodb-stitch-browser-sdk";
 
 
 class Demo extends React.Component {
@@ -72,19 +28,24 @@ class Demo extends React.Component {
   componentDidMount() {
     console.log("component Did mount running");
   }
-  
+
   setupStitch() {
     console.log("setup stitch");
     const appName = 'authentication_test-htbrq';
     const stitchClient = Stitch.hasAppClient(appName) ? Stitch.defaultAppClient : Stitch.initializeDefaultAppClient(appName);
+    
     if (stitchClient.auth.hasRedirectResult()) {
         stitchClient.auth.handleRedirectResult().then(user => {
-        console.log(user);
+        this.setState({
+          "user": user, 
+        });
     })}
+
     if (!stitchClient.auth.isLoggedIn) {
       const credential = new GoogleRedirectCredential();
       Stitch.defaultAppClient.auth.loginWithRedirect(credential);
     }
+
   }
 
 
@@ -97,62 +58,14 @@ class Demo extends React.Component {
     console.log(this.state);
   }
 
-  //this.propTypes = {
-  //  classes: PropTypes.object.isRequired,
-  //};
-
-
   render() {
 
-  return (
-    <React.Fragment>
-      <div className="login">
-      <div className="login-container">
-      <CssBaseline />
-      <main className={classes.layout}>
-        <Paper className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form className={classes.form}>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="email">Email Address</InputLabel>
-              <Input id="email" onChange={this.handleChange} value={this.state.email} name="email" autoComplete="email" autoFocus />
-            </FormControl>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="password">Password</InputLabel>
-              <Input
-                name="password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-            </FormControl>
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              component="button"
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign in
-            </Button>
-          </form>
-        </Paper>
-      </main>
+    return (
+      <div className="user">
+         <pre>{this.state.user}</pre>
       </div>
-      </div>
-    </React.Fragment>
-  )
-}
+    )
+  }
 }
 
 // ========================================
