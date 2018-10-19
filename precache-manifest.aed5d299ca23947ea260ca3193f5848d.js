bc7343a0e0ddfcb9ec55750f5fc0ab82ba95cc3b
skerschb <sue.kerschbaumer@10gen.com>
2018-10-19 01:54:26 -0400

    Updates

---
+++ precache-manifest.aed5d299ca23947ea260ca3193f5848d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a03b0da128bf13017203",
+    "url": "/testauth/static/js/runtime~main.a03b0da1.js"
+  },
+  {
+    "revision": "df1948e7d362c12429e4",
+    "url": "/testauth/static/js/main.df1948e7.chunk.js"
+  },
+  {
+    "revision": "0420dc158754e6f3897d",
+    "url": "/testauth/static/js/1.0420dc15.chunk.js"
+  },
+  {
+    "revision": "df1948e7d362c12429e4",
+    "url": "/testauth/static/css/main.c1785159.chunk.css"
+  },
+  {
+    "revision": "2dd31870d08c4d6623eff4281b9e5211",
+    "url": "/testauth/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/testauth/precache-manifest.8d949f346d3b73aba50eb55247478017.js"
+  "/testauth/precache-manifest.aed5d299ca23947ea260ca3193f5848d.js"
 );
 
 workbox.clientsClaim();
